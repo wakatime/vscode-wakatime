eb66b460336c27dd0ba66f5175ceb6df12035f2e
Alan Hamlett <alanhamlett@users.noreply.github.com>
2021-09-14 10:18:48 -0700

    Stop writing ini file directly

--- src/wakatime.ts
+++ src/wakatime.ts
@@ -61,16 +61,7 @@ export class WakaTime {
     this.statusBar.show();
 
     this.setupEventListeners();
-
-    // enable auto config for online IDE like gitpods
-    if (process.env.WAKA_TOKEN) {
-      console.log('WAKA_TOKEN enabled')
-      if (!fs.existsSync(this.options.getConfigFile()) || !fs.readFileSync(this.options.getConfigFile()).toString().includes("api_key")) {
-        // not using this.options.setSetting, because it's async
-        fs.writeFileSync(this.options.getConfigFile(), `[settings]\napi_key = ${process.env.WAKA_TOKEN}`)
-        console.log('set env finished')
-      }
-    } 
+    
     this.options.getSetting('settings', 'disabled', (disabled: Setting) => {
       this.disabled = disabled.value === 'true';
       if (this.disabled) {
