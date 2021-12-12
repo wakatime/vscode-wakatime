be06c3b37090641aff5e4b028933124904f0b3cc
Alan Hamlett <alan.hamlett@gmail.com>
2021-12-12 13:13:02 -0800

    prevent variable being unused

--- src/web/extension.ts
+++ src/web/extension.ts
@@ -1,6 +1,6 @@
 import * as vscode from 'vscode';
 
-export function activate(ctx: vscode.ExtensionContext) {
+export function activate(_: vscode.ExtensionContext) {
   console.log('Loaded the extension.');
 }
 
