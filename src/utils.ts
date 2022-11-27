75c0b46f83ed082df05ae2ace8deaa7b16a118e8
Alan Hamlett <alan.hamlett@gmail.com>
2022-11-27 19:31:36 +0100

    Support waka api key prefix

--- src/utils.ts
+++ src/utils.ts
@@ -8,7 +8,7 @@ export class Utils {
     const err = 'Invalid api key... check https://wakatime.com/settings for your key';
     if (!key) return err;
     const re = new RegExp(
-      '^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$',
+      '^(waka_)?[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$',
       'i',
     );
     if (!re.test(key)) return err;
